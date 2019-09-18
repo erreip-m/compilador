4ce541f53db5405284fdd30b2cc3ee3a98d74d9b
erreip-m <55513086+erreip-m@users.noreply.github.com>
2019-09-18 18:06:44 -0300

    Add files via upload

---
+++ Lexer.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+
+// Lista de palavras reservadas
+//
+//      programainicio, execucaoinicio, fimexecucao, fimprograma
+//
+//      definainstrucao, como
+//
+//      inicio, fim
+//
+//      repita, vezes, fimrepita
+//
+//      enquanto, faca, fimpara
+//
+//      se, ent ao, fimse, senao, fimsenao
+//
+//      mova, passo, passos, vire para, pare, finalize, apague lampada, acenda lampada, aguarde ate
+//
+//      robo pronto, robo ocupado, robo parado, robo movimentando, frente robo bloqueada, 
+//      direita robo bloqueada, esquerda robo bloqueada, lampada acesa a frente, 
+//      lampada apagada a frente, lampada acesa a esquerda,  lampada apagada a esquerda,
+//      lampada apagada a esquerda, lampada acesa a direita, lampada apagada a direita
+//      
+//      esquerda, direita
+
+// Verifica se o caractere é um digito
+bool digito(char t) {
+    return (t >= '0'  &&  t <= '9');
+}
+
+// Verifica se o caractere é uma letra
+bool letra(char t) {
+    return ((t >= 'a'  &&  t<='z')  ||  (t >= 'A'  &&  t <= 'Z'));
+}
+
+// Verifica se o caractere é de espaçamento
+bool espaco(char t) {
+    return (t == ' '  ||  t == '\t'  ||  t == '\n');
+}
+
+//TODO dividir em inicio de identifiador e resto
+bool caractereValido(char t) {
+    return (digito(t)  ||  letra(t)  ||  espaco(t));
+}
+
+
+
+int main() {
+    (letra('a')? printf("true\n") : printf("false\n"));
+}
