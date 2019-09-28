787d7936cc47e9094a4ac8d0cea7300f7ba787b0
erreip-m <pierremmelhor@gmail.com>
2019-09-28 14:40:51 -0300

    Classe Token

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,4 +1,10 @@
-#include <ios>
+#include <iostream>
+#include <set>
+
+using std::set;
+using std::string;
+using std::cin;
+using std::cout;
 
 // Lista de palavras reservadas
 
@@ -23,6 +29,132 @@
       
 //      esquerda, direita
 
+class Token {
+    public:
+        const set<string> reservadas {
+            "A",
+            "ACENDA",
+            "ACESA",
+            "AGUARDE",
+            "APAGADA",
+            "APAGUE",
+            "ATE",
+            "BLOQUEADA",
+            "COMO",
+            "DEFINAINSTRUCAO",
+            "DIREITA",
+            "ENQUANTO",
+            "ENTAO",
+            "ESQUERDA",
+            "EXECUCAOINICIO",
+            "FACA",
+            "FIM",
+            "FIMEXECUCAO",
+            "FIMPARA",
+            "FIMPROGRAMA",
+            "FIMREPITA",
+            "FIMSE",
+            "FIMSENAO",
+            "FINALIZE",
+            "FRENTE",
+            "INICIO",
+            "LAMPADA",
+            "MOVA",
+            "MOVIMENTANDO",
+            "OCUPADO",
+            "PARA",
+            "PARADO",
+            "PARE",
+            "PASSO",
+            "PASSOS",
+            "PROGRAMAINICIO",
+            "PRONTO",
+            "REPITA",
+            "ROBO",
+            "SE",
+            "SENAO",
+            "VEZ",
+            "VEZES",
+            "VIRE"
+        };
+        enum class Tipo {
+            Comentario,
+            Identificador,
+            Numero, 
+            Reservado
+          /*Reservados:
+            A,
+            ACENDA,
+            ACESA,
+            AGUARDE,
+            APAGADA,
+            APAGUE,
+            ATE,
+            BLOQUEADA,
+            COMO,
+            DEFINAINSTRUCAO,
+            DIREITA,
+            ENQUANTO,
+            ENTAO,
+            ESQUERDA,
+            EXECUCAOINICIO,
+            FACA,
+            FIM,
+            FIMEXECUCAO,
+            FIMPARA,
+            FIMPROGRAMA,
+            FIMREPITA,
+            FIMSE,
+            FIMSENAO,
+            FINALIZE,
+            FRENTE,
+            INICIO,
+            LAMPADA,
+            MOVA,
+            MOVIMENTANDO,
+            OCUPADO,
+            PARA,
+            PARADO,
+            PARE,
+            PASSO,
+            PASSOS,
+            PROGRAMAINICIO,
+            PRONTO,
+            REPITA,
+            ROBO,
+            SE,
+            SENAO,
+            VEZ,
+            VEZES,
+            VIRE*/
+        };
+        Token(Tipo tipo, string lexema);
+        Tipo obterTipo();
+        string obterLexema();
+
+    private:
+        Tipo tipo;
+        string lexema;
+};
+
+Token::Token(Tipo tipo, string lexema) {
+    this->tipo = tipo;
+    this->lexema = lexema;
+}
+
+Token::Tipo Token::obterTipo() {
+    return tipo;
+}
+
+string Token::obterLexema() {
+    return lexema;
+}
+
+class Lexer {
+    private:
+        int *ptr;
+};
+
 // Verifica se o caractere é um digito
 bool digito(char t) {
     return (t >= '0'  &&  t <= '9');
@@ -43,8 +175,8 @@ bool caractereValido(char t) {
     return (digito(t)  ||  letra(t)  ||  espaco(t));
 }
 
-//TODO função para "espiar" próximo char da cadeia e outra para pegá-lo
 
 int main() {
-    std::cout<<"teste";
+    Token tok(Token::Tipo::Identificador, "teste123");
+    cout << tok.obterLexema() << "\n";
 }
